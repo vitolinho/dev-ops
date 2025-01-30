469b787054ea874157b8c51a5f1f71d5559fd617
JinSu77 <adrien2098@hotmail.fr>
2025-01-30 16:04:57 +0100

    remove metrics from api

--- api/internal/app/router/router.go
+++ api/internal/app/router/router.go
@@ -2,14 +2,13 @@ package router
 
 import (
 	"porsche-api/internal/app/handler"
-	"porsche-api/internal/middleware"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func MakeRoutes(app *fiber.App) {
-	api := app.Group("/api", logger.New(), middleware.MetricsMiddleware())
+	api := app.Group("/api", logger.New())
 
 	v1 := api.Group("/v1")
 	v1.Post("/cars", handler.AddCar)
