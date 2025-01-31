3e0201673cf3b501823b6d2786571da16041f59f
vitolinho <93949338+vitolinho@users.noreply.github.com>
2025-01-31 02:52:16 +0100

    refactor : update BACKUP_DIR & source

--- api/scripts/backup.sh
+++ api/scripts/backup.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
-
-# Variables
-
-source ../.env
-
-
-BACKUP_DIR="../backups"
+source ./.env
+BACKUP_DIR="./backups"
 LOG_FILE="$BACKUP_DIR/backup.log"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 BACKUP_FILE="$BACKUP_DIR/backup_$DATE.sql"
-
 mkdir -p "$BACKUP_DIR"
-
 export PGPASSWORD="$DB_PASSWORD"
 pg_dump --username="$DB_USER" --host="$DB_HOST" --port="$DB_PORT" --dbname="$DB_NAME" --format=plain --file="$BACKUP_FILE"
 EXIT_CODE=$?
-unset PGPASSWORD  # Sécurisation du mot de passe après utilisation
-
-
+unset PGPASSWORD
 if [ $EXIT_CODE -eq 0 ]; then
     echo "[$(date +"%Y-%m-%d %H:%M:%S")] Backup PostgreSQL réussi : $BACKUP_FILE" | tee -a "$LOG_FILE"
 else
     echo "[$(date +"%Y-%m-%d %H:%M:%S")] ERREUR: La sauvegarde a échoué !" | tee -a "$LOG_FILE"
     exit 1
 fi
-
